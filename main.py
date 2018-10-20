cbf2db833b4d9e8373e96bcccca8e6aefe2f82bf
Kristian Sakarisson <kristian@sakarisson.com>
2018-10-20 12:42:20 +0300

    Added color matching

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ from time import sleep
 # Connect to sensor
 sensor = ColorSensor()
 
-sensor.mode = 'COL-REFLECT'
+sensor.mode = 'COL-COLOR'
+
+colors=('unknown','black','blue','green','yellow','red','white','brown')
 
 while True:
-  print(sensor.value())
+  print(colors[sensor.value()])
   sleep(0.5)
