09c3eeb72858c62f33ed5d8e825f13b661a00589
Kristian Sakarisson <kristian@sakarisson.com>
2018-10-20 12:35:05 +0300

    Added basic color sensor test

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+
+from ev3dev.ev3 import ColorSensor
+from time import sleep
+
+# Connect to sensor
+sensor = ColorSensor()
+
+sensor.mode = 'COL-REFLECT'
+
+while True:
+  print(sensor.value())
+  sleep(0.5)
